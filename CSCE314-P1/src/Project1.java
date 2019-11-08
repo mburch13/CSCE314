d6b9b656ee07778c291a807a18fc91b2f374bfe3
McKenzie <46796707+mburch13@users.noreply.github.com>
2019-11-08 00:18:47 -0600

    Update Project1.java
    Refactoring:
    Primes -> PrimeOperations
    p -> testOne
    testtwo -> testTwo

--- CSCE314-P1/src/Project1.java
+++ CSCE314-P1/src/Project1.java
@@ -2,20 +2,20 @@ public class Project1 {
 	public static void main(String[] args) 
 	{
 		// Instantiate Primes Class
-		Primes p = new Primes();
+		PrimeOperations testOne = new PrimeOperations();
 		
 		// Generate Primes and test.
-		p.generatePrimes(21);
-		p.printPrimes();
+		testOne.generatePrimes(21);
+		testOne.printPrimes();
 		
 		// Generate and test Twin Primes
-		Primes testtwo = new Primes();
-		testtwo.generatePrimes(100);
-		testtwo.generateTwinPrimes();
-		testtwo.printTwins();
+		PrimeOperations testTwo = new PrimeOperations();
+		testTwo.generatePrimes(100);
+		testTwo.generateTwinPrimes();
+		testTwo.printTwins();
 		
 		// Generate and test Hexagonal crosses
-		Primes testThree = new Primes();
+		PrimeOperations testThree = new PrimeOperations();
 		testThree.generatePrimes(2000);
 		testThree.generateTwinPrimes();
 		testThree.generateHexPrimes();

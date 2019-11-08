0d2f0c6c3b324c90ed48b06759f997a3c86e3c29
McKenzie <46796707+mburch13@users.noreply.github.com>
2019-11-08 00:23:21 -0600

    Update PrimeOperations.java
    function comments and working on generatingPrimes function

--- CSCE314-P1/src/PrimeOperations.java
+++ CSCE314-P1/src/PrimeOperations.java
@@ -15,35 +15,73 @@ public class PrimeOperations {
 	// Add a prime to the prime list if and only iff it is not already in the list. (ignore duplicates)
 	public void addPrime(BigInteger x)
 	{
+		//if list is null add to list
+				//else check to see if number is in list
+					//if present return
+					//else add to list
 	}
 	
 	// Output the prime list. Each prime should be on a separate line and the total number of primes should be on the following line.
 	public void printPrimes()
 	{
+		//call and println generatePrimes
+//		System.out.println(generatePrimes(count));
 	}
 		
 	// Output the twin prime list. Each twin prime should be on a separate line with a comma separating them, and the total number of twin primes should be on the following line.
 	public void printTwins()
 	{
+		//call and println generateTwinPrimes
+
 	}
 		
 	// Output the hexagon cross list. Each should be on a separate line listing the two twin primes and the corresponding hexagon cross, with the total number on the following line.
 	public void printHexes()
 	{
+		//call and println generateHexPrimes
+
 	}
 		
 	// Generate and store a list of primes.
 	public void generatePrimes(int count)
 	{
+		/**
+		 * for loop for all possible primes{
+		 * 	if count not 0{
+		 * 		for loop to start checking if number is prime{
+		 * 			if mod2 == 0 
+		 * 		}addPrime()
+		 * 	}--count
+		 * }*/
+		
+		for(BigInteger x = new BigInteger("2"); x.compareTo(BigInteger.ZERO) > 0; x = x.add(BigInteger.ONE)) {
+			if(count != 0) {
+				System.out.println("X Before Check: " + x);
+				for(BigInteger i = new BigInteger("2"); i.compareTo(x.divide(new BigInteger("2"))) <= 0; i = i.add(BigInteger.ONE)) {
+					if(x.mod(i).equals(BigInteger.ZERO)) {
+						break; //number NOT a prime
+					}
+					System.out.println("X After Check: " + x);
+					
+					//addPrime to the list
+				}
+				--count;
+				System.out.println("Test: " + count);
+			}
+		}
 	}
 	
 	// Generate and store a list of twin primes.
 	public void generateTwinPrimes()
 	{
+		//call generatePrimes
+				//while 
 	}
 	
 	// Generate and store the hexagon crosses, along with the two twin primes that generate the hexagon cross.
 	public void generateHexPrimes()
 	{
+		//loop through list
+		//if twin prime
 	}
 }

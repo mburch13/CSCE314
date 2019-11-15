cfb861c01adcf1d4278ab3c64dfbabe8fd1f8e3d
McKenzie <46796707+mburch13@users.noreply.github.com>
2019-11-15 09:06:49 -0600

    Update Project1.java
    Fixed formating and generatePrimeHex to allow for print statements to match

--- CSCE314-P1/src/Project1.java
+++ CSCE314-P1/src/Project1.java
@@ -1,9 +1,13 @@
+//McKenzie Burch
+//CSCE 314-501
+//GitHub: https://github.com/mburch13/CSCE314-PHASE1.git
 import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class Project1 {
 	public static void main(String[] args) 
 	{
+		try {
 		// Instantiate Primes Class
 		PrimeOperations testOne = new PrimeOperations();
 
@@ -16,12 +20,25 @@ public class Project1 {
 		testTwo.generatePrimes(100);
 		testTwo.generateTwinPrimes();
 		testTwo.printTwins();
-		
-		// Generate and test Hexagonal crosses
+//		
+//		// Generate and test Hexagonal crosses
 		PrimeOperations testThree = new PrimeOperations();
 		testThree.generatePrimes(2000);
 		testThree.generateTwinPrimes();
 		testThree.generateHexPrimes();
 		testThree.printHexes();
+		
+//		System.out.println("\n MY TEST CASES");
+//		PrimeOperations myTest = new PrimeOperations();
+//		myTest.generatePrimes(40);
+//		myTest.printPrimes();
+//		myTest.generateTwinPrimes();
+//		myTest.printTwins();
+//		myTest.generateHexPrimes();
+//		myTest.printHexes();
+	
+		} catch(RuntimeException ex) {
+			System.out.println("Runtime Error");
+		}
 	}
 }

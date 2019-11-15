20b87025c2cb33941e66c48cd61b1e5768c03f38
McKenzie <46796707+mburch13@users.noreply.github.com>
2019-11-15 09:11:33 -0600

    Update PrimeOperations.java
    fixed formating and generatePrimeHex to allow for printing to match given test cases

--- CSCE314-P1/src/PrimeOperations.java
+++ CSCE314-P1/src/PrimeOperations.java
@@ -1,3 +1,6 @@
+//McKenzie Burch
+//CSCE 314-501
+//GitHub: https://github.com/mburch13/CSCE314-PHASE1.git
 import java.util.ArrayList; 
 import java.math.BigInteger;
 
@@ -8,14 +11,14 @@ public class PrimeOperations {
 	
 	// Pair class implementation.
 	private class Pair<T> {
-		T x, y;
+		T x;
+		T y;
 		
 		//constructor
 		public Pair(T x1, T y1) {
 			this.x = x1;
 			this.y = y1;
 		}
-
 
 		public T getX() {
 			return x;
@@ -42,14 +45,13 @@ public class PrimeOperations {
 	// Member variables for containing out lists of integers, twin primes, hexagon crosses, and the pairs of twin primes that make up the hex crosses.
 	ArrayList<BigInteger> primes = new ArrayList<BigInteger>();
 	ArrayList<Pair<BigInteger>> primeTwins = new ArrayList<Pair<BigInteger>>();
-	ArrayList<Pair> primePairs = new ArrayList<Pair>();
 	ArrayList<Pair> primeHex = new ArrayList<Pair>();
+	ArrayList<Pair> primeHexSplit = new ArrayList<Pair>();
 
 	// Add a prime to the prime list if and only iff it is not already in the list. (ignore duplicates)
 	public void addPrime(BigInteger x)
 	{
-		/* make an array list
-		 * if list is null{
+		/* if list is null{
 		 * 	add to list
 		 * 	}else{
 		 * 		check to see if number is in list
@@ -62,12 +64,10 @@ public class PrimeOperations {
 
 		if(primes.isEmpty()) {
 			primes.add(x);
-//			System.out.println(x + " was added to list");
 		}
 		else {
 			if (primes.contains(x) != true) {
 				primes.add(x);
-//				System.out.println(x + " was added to list");
 			}
 		}
 	}
@@ -75,8 +75,6 @@ public class PrimeOperations {
 	// Output the prime list. Each prime should be on a separate line and the total number of primes should be on the following line.
 	public void printPrimes()
 	{
-//		System.out.print("hello");
-//		System.out.println(primes.size());
 		for(int i = 0; i < primes.size(); i++) {
 			System.out.println(primes.get(i));
 		}
@@ -87,12 +85,8 @@ public class PrimeOperations {
 	// Output the twin prime list. Each twin prime should be on a separate line with a comma separating them, and the total number of twin primes should be on the following line.
 	public void printTwins()
 	{
-		//call and println generateTwinPrimes
-		
-//		for(PrimeOperations.Pair<BigInteger> i: primeTwins) {
 		for(int i = 1; i < primeTwins.size(); i++) {
 			System.out.println(primeTwins.get(i));
-//			System.out.println(i);
 		}
 		System.out.println("Total Twins: " + primeTwins.size());
 
@@ -101,9 +95,8 @@ public class PrimeOperations {
 	// Output the hexagon cross list. Each should be on a separate line listing the two twin primes and the corresponding hexagon cross, with the total number on the following line.
 	public void printHexes()
 	{
-		//call and println generateHexPrimes
 		for(int i = 0; i < primeHex.size(); i++) {
-			System.out.println("Prime Pairs: " + primePairs.get(i).getX() + " and " + primePairs.get(i).getY() + " are seperated by " + primeHex.get(i).getX());
+			System.out.println("Prime Pairs: " + primeHex.get(i).getX() + " and " + primeHex.get(i).getY() + " separated by " + primeHexSplit.get(i));
 		}
 		System.out.println("Total Hex Crosses: " + primeHex.size());
 
@@ -130,28 +123,24 @@ public class PrimeOperations {
 		 * }*/
 		
 		int flag = -1; //change if number is prime
-		BigInteger start = new BigInteger("2");
-//		BigInteger amount = BigInteger.valueOf(count);
+		BigInteger num = BigInteger.ONE;
 		
-		for(int amount = count; amount > 0; start = start.add(BigInteger.ONE)) {
-//			System.out.println("X Before Check: " + x);
-			if(start.compareTo(new BigInteger("2")) == 0 || start.compareTo(new BigInteger("3")) == 0) {
+		for(int amount = count; amount > 0; num = num.add(BigInteger.ONE)) {
+			if(num.compareTo(new BigInteger("2")) == 0 || num.compareTo(new BigInteger("3")) == 0) {
 				flag = 1;
 			}
-			for(BigInteger i = new BigInteger("2"); i.compareTo(start.divide(new BigInteger("2"))) <= 0; i = i.add(BigInteger.ONE)) {	
-				if(start.mod(i).equals(BigInteger.ZERO)) {
-				flag = -1;
-					break; //number NOT a prime
+			for(BigInteger i = new BigInteger("2"); i.compareTo(num.divide(new BigInteger("2"))) <= 0; i = i.add(BigInteger.ONE)) {	
+				if(num.mod(i).equals(BigInteger.ZERO)) {
+					//number NOT a prime
+					flag = -1;
+					break; 
 				}
-//				System.out.println("X After Check: " + x);
 				flag = 1;
 			}
 			if(flag == 1) {
 				//addPrime to the list
-				addPrime(start);
+				addPrime(num);
 				amount--;
-//				System.out.print(start);
-//				System.out.println("\tTest: " + amount);
 			}
 		}
 	}
@@ -172,14 +161,9 @@ public class PrimeOperations {
 		
 		for(int i = 1; i < primes.size(); i++) {
 			BigInteger diff = primes.get(i).subtract(primes.get(i-1));
-//			System.out.print(primes.get(i-1) + ", " + primes.get(i));
-//			System.out.println("\t" + diff);
 			if(diff.compareTo(new BigInteger("2")) == 0){
-//				System.out.println("adding to the list");
 				PrimeOperations.Pair<BigInteger> twins = new Pair<BigInteger>(primes.get(i-1), primes.get(i));
 				primeTwins.add(twins);
-//				primeTwins.add(primes.get(i));
-//				primeTwins.add(primes.get(i-1));
 			}
 		}
 	}
@@ -194,34 +178,28 @@ public class PrimeOperations {
 		 * between the twin prime 11 and 13, and 6 * 2 = 12. (6,270 and 12540 are another hexagon cross.)
 		 * 
 		 * loop through primeTwins i as iterator{
-		 * 	loop through all numbers x as iterator{
-		 * 		if i-x == 1{
-		 * 			loop through all numbers z as iterator{
-		 * 				if (i-1) - z == 1{
-		 * 					if z*2 == x{
-		 * 						add to list
-		 * 					}
-		 * 				}
-		 * 			}
+		 * 	loop through primeTwins k as iterator{
+		 * 		if (i+1)*2 == k{
+		 * 			create pairs to store prime pairs and hex crosses			
+		 * 			add to list
 		 * 		}
 		 * 	}
 		 * }			
 		 */
 		
 		for(int i = 0; i < primeTwins.size(); i ++) {
-//			PrimeOperations.Pair<BigInteger> hexCross = new Pair(primeTwins.get(i), primeTwins.get(i+1));
 			for(int k = 0; k < primeTwins.size(); k++) {
-				if(primeTwins.get(i).getX().add(BigInteger.ONE).multiply(new BigInteger ("2")).compareTo(primeTwins.get(k).getX().add(BigInteger.ONE)) == 0) {
+				BigInteger t1 = primeTwins.get(i).getX().add(BigInteger.ONE);
+				BigInteger t2 = primeTwins.get(k).getX().add(BigInteger.ONE);
+				if(t1.multiply(new BigInteger ("2")).compareTo(t2) == 0) {
 					Pair<BigInteger> p1 = new Pair<BigInteger>(primeTwins.get(i).getX(), primeTwins.get(i).getY());
 					Pair<BigInteger> p2 = new Pair<BigInteger>(primeTwins.get(k).getX(), primeTwins.get(k).getY());
-					Pair<BigInteger> s1 = new Pair<BigInteger>(primeTwins.get(i).getX().add(BigInteger.ONE), (primeTwins.get(k).getX().add(BigInteger.ONE)));
-					Pair<BigInteger> s2 = new Pair<BigInteger>(primeTwins.get(i+1).getX().add(BigInteger.ONE), (primeTwins.get(k+1).getX().add(BigInteger.ONE)));
 					
 					Pair<Pair> pairs = new Pair<Pair>(p1, p2);
-					primePairs.add(pairs);
+					primeHex.add(pairs);
 					
-					Pair<Pair> splits = new Pair<Pair>(s1, s2);
-					primeHex.add(splits);
+					Pair<BigInteger> splits = new Pair<BigInteger>(t1, t2);
+					primeHexSplit.add(splits);
 				}
 			}
 		}

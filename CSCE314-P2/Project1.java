9b6a21d704e75e3a04ad8e6a6bf9cd78c38741a0
McKenzie <46796707+mburch13@users.noreply.github.com>
2019-11-22 10:05:20 -0600

    Delete Project1.java

--- CSCE314-P2/Project1.java
+++
@@ -1,9 +0,0 @@
-public class Project1 {
-	public static void main(String[] args) 
-	{
-		// Instantiate Primes Class
-		Primes p = new Primes();
-		
-		MainWindow mw = new MainWindow(Config.APPLICATIONNAME, p);
-	}
-}

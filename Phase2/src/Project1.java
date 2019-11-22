b1f220ca2bef58ecd2d9968726bb6b48d80756e0
McKenzie <46796707+mburch13@users.noreply.github.com>
2019-11-22 10:17:50 -0600

    Create Project1.java

---
+++ Phase2/src/Project1.java
@@ -0,0 +1,9 @@
+public class Project1 {
+	public static void main(String[] args) 
+	{
+		// Instantiate Primes Class
+		Primes p = new Primes();
+		
+		MainWindow mw = new MainWindow(Config.APPLICATIONNAME, p);
+	}
+}

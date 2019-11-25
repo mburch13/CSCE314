4cbda7163cbc6627829e49df418ed6a01dd07d34
McKenzie <46796707+mburch13@users.noreply.github.com>
2019-11-25 16:33:51 -0600

    Update FileAccess.java
    installed jdk9, added commets that includes requirements from pdf

--- Phase2/src/FileAccess.java
+++ Phase2/src/FileAccess.java
@@ -1,4 +1,14 @@
 // This file gives access to the underlying datafile and stores the data in the Workout class.
+/*
+ * create static functions to load/save primes and crosses
+ * return true if the operation succeeds and false if it fails
+ * success or failure should be used to update the main window’s status bar 
+ * save functions must use the “ for-each ” Java language construct and iterators to work through 
+ * the prime and cross lists. 
+ * All saves and loads should be to the “data” subdirectory, but should allow 
+ * user-specified filenames from the GUI. This custom filename ability can allow the user to specify 
+ * additional path information as part of the custom filename.
+ */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;

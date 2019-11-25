7f22e7b54038948582ede3a723d8fb0bbab7a586
McKenzie <46796707+mburch13@users.noreply.github.com>
2019-11-25 16:33:42 -0600

    Update Config.java
    installed jdk9, added commets that includes requirements from pdf

--- Phase2/src/Config.java
+++ Phase2/src/Config.java
@@ -2,6 +2,9 @@
 
 public class Config {
   public final static String DATAPATH = "data/"; // If you don't know what the static keyword does, you better go look it up now.
+  public final static String APPLICATIONNAME = "Hexagon Cross Generator";
   // Prime file: One prime per line.
+  public String primeFile = "primes.txt"; 
   // Cross file: Two primes per line, separated by a comma.
+  public String crossFile = "crosses.text";
 }

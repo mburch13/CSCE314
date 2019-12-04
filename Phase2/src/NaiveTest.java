64700d804c1170fa217c78e39fb5d349e592cd5a
McKenzie <46796707+mburch13@users.noreply.github.com>
2019-12-03 19:20:38 -0600

    Update NaiveTest.java

--- Phase2/src/NaiveTest.java
+++ Phase2/src/NaiveTest.java
@@ -1,5 +1,7 @@
 import java.math.BigInteger;
 
+import javax.swing.plaf.basic.BasicIconFactory;
+
 /*
  * A NaiveTest.java file is included with a simple, functional isPrime function. 
  * replace isPrime function with a more efficient test for primality. 
@@ -18,13 +20,16 @@ public class NaiveTest
 	public static boolean isPrime(BigInteger candidate)
 	{
 		if (!candidate.isProbablePrime((100))) return false; // Weed out the likely not primes.
-		
-		BigInteger loopStop = candidate.sqrt().add(BigInteger.ONE);
-		for (BigInteger outerIndex = BigInteger.TWO; !outerIndex.equals(loopStop); outerIndex = outerIndex.add(BigInteger.ONE))
-		{
-			for (BigInteger innerIndex = outerIndex; !innerIndex.equals(loopStop); innerIndex = innerIndex.add(BigInteger.ONE))
-			{
-				if (candidate.equals(innerIndex.multiply(outerIndex))) return false;
+		//used prime finder from part 1 of my project
+		for(int amount = 0; amount > 0; candidate = candidate.add(BigInteger.ONE)) {
+			if(candidate.compareTo(BigInteger.TWO) == 0 || candidate.compareTo(new BigInteger("3")) == 0) {
+				return true;
+			}
+			for(BigInteger i = BigInteger.TWO; i.compareTo(candidate.divide(new BigInteger("2"))) <= 0; i = i.add(BigInteger.ONE)) {	
+				if(candidate.mod(i).equals(BigInteger.ZERO)) {
+					//number NOT a prime
+					return false; 
+				}
 			}
 		}
 		return true;
